30d7971eb01b5e04857546989ed52eb88313eef4
Sinan Koyuncu <koyuncusinan1@gmail.com>
2023-07-17 15:15:01 +0300

    Middleware change

--- server/routes/route-recipe.js
+++ server/routes/route-recipe.js
@@ -6,9 +6,9 @@ import {verifyToken} from "../middlewares/middle-auth.js"
 
 const router = express.Router()
 router.get("/", invoke)
-router.get("/community/recipes", verifyToken, getRecipe)
+router.get("/community/recipes", getRecipe)
 
-router.get("/community/recipes/:id", verifyToken, singleRecipe)
+router.get("/community/recipes/:id", singleRecipe)
 
 router.post("/community/recipes/:id",bookmarked)
 router.post("/community/create", upload.single("picturePath"), verifyToken, createRecipe)
